51f2bac49f731e409d6d7857ae297252fc1d65f3
mrmsoares <marcosroberto.ms@gmail.com>
2021-06-09 18:35:55 -0300

    Métodos HTTP

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
+const { request, response } = require("express");
 const express = require("express");
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: "Hello World Motherfuckers!" });
-})
+app.get('/courses', (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4", "Curso 5"]);
+});
+
+app.post('/courses', (request, response) => {
+    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4", "Curso 5", "Curso 6"]);
+});
+
+app.put('/courses/:id', (request, response) => {
+    return response.json(["Curso 7", "Curso 2", "Curso 3", "Curso 4", "Curso 5", "Curso 8"]);
+});
+
+app.delete('/courses/:id', (request, response) => {
+    return response.json(["Curso 2", "Curso 3", "Curso 4", "Curso 5"]);
+});
 
 app.listen(3333);
 
