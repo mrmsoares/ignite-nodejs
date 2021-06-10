df6bcb31d539c196a5a2fea5c9c4ef4e112fd29d
mrmsoares <marcosroberto.ms@gmail.com>
2021-06-10 13:32:59 -0300

    Modificando o index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,29 @@
-const { request, response } = require("express");
 const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/courses', (request, response) => {
+    const query = request.query;
+
+    console.log(query);
+
     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4", "Curso 5"]);
 });
 
 app.post('/courses', (request, response) => {
+    const body = request.body;
+
+    console.log(body);
+
     return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4", "Curso 5", "Curso 6"]);
 });
 
 app.put('/courses/:id', (request, response) => {
+    const { id } = request.params;
+    console.log(id);
+
     return response.json(["Curso 7", "Curso 2", "Curso 3", "Curso 4", "Curso 5", "Curso 8"]);
 });
 
